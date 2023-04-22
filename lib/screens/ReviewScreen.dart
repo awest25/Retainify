8c3b5834f06a907d9c6c45023c88efe1bb4a4bbf
Caleb Yang <kb@jaymoo.com>
2023-04-22 01:43:59 -0700

    Set foundation for Review Screen

--- lib/screens/ReviewScreen.dart
+++ lib/screens/ReviewScreen.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:retainify/screens/NotesScreen.dart';
 
-class ReviewScreen extends StatelessWidget {
-  const ReviewScreen({super.key});
+class ReviewScreen extends StatefulWidget {
+  final String question;
+
+  ReviewScreen({required this.question});
+
+  @override
+  _ReviewScreenState createState() => _ReviewScreenState();
+}
+
+class _ReviewScreenState extends State<ReviewScreen> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Center(
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.pop(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const NotesScreen()));
-                    },
-                    child: const Text("Notes Screen")))));
+      appBar: AppBar(
+        title: const Text('Review Question'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              widget.question,
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                hintText: 'Type your answer here',
+              ),
+              maxLines: 3,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Save the user's answer and go to the next question
+                String answer = _textEditingController.text;
+                // TODO: Implement logic for saving answer and going to next question
+              },
+              child: const Text('Submit Answer'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

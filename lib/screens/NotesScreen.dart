5cd53c01551b44e04616adddf10554aa293aaef9
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-04-22 02:59:08 -0700

    expansion tiles with titles and styling

--- lib/screens/NotesScreen.dart
+++ lib/screens/NotesScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import "package:retainify/components/NotesListItem.dart";
 import "package:retainify/screens/ReviewScreen.dart";
+import "package:retainify/components/NotesListItem.dart";
+import 'package:intl/intl.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
@@ -7,22 +10,48 @@ class NotesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: const Text("Notes")),
         body: Container(
-            child: Center(
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const ReviewScreen(questions: [
-                                    'Who was the first President of the United States?',
-                                    'Who was the second President of the United States?',
-                                    'Who was the third President of the United States?',
-                                    'Who was the fourth President of the United States?',
-                                    'Who was the fifth President of the United States?',
-                                  ])));
-                    },
-                    child: const Text("Review Screen")))));
+            child: ListView(children: [
+          _tile("Physics - Velocity and Displacement", 1),
+          _tile("Math - Pythagorean Theorem", 4)
+        ])));
   }
 }
+
+Widget _tile(String title, int daysSinceImport) {
+  final header1 = TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+  final header2 = TextStyle(fontSize: 18);
+  double percent = 0;
+  String nextDate = "";
+
+// TODO do this logic for next deadline and percent until that date
+  if (daysSinceImport > 35) {}
+
+  return Card(
+    child: ExpansionTile(
+        title: Text(title, style: header1),
+        trailing: CircularProgressIndicator(value: percent),
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Next Retainify', style: header2),
+                  Text(nextDate, style: header2),
+                ],
+              ))
+        ]),
+  );
+}
+
+// child: Center(
+//     child: TextButton(
+//         onPressed: () {
+//           Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                   builder: (context) => const ReviewScreen()));
+//         },
+//         child: const Text("Go to Review Screen")))

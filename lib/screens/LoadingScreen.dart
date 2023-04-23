40ac9077d148d366541edb596450e205e3fdfaea
Alex West <awest2025@ucla.edu>
2023-04-23 09:45:57 -0700

    changed loading screen to use loading circle

--- lib/screens/LoadingScreen.dart
+++ lib/screens/LoadingScreen.dart
@@ -16,11 +16,7 @@ class LoadingScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width * 0.9,
               child: ListView(children: const [
                 SizedBox(height: 20),
-                Text(
-                  "Loading...",
-                  style: TextStyle(
-                      fontSize: 35, color: Color.fromARGB(255, 133, 133, 133)),
-                ),
+                Center(child: CircularProgressIndicator()),
               ]))),
     );
   }
